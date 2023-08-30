976994e5ff6c8c47ded4313675e0d659139e25e0
yuri <yurifreitas@doutor123.com.br>
2023-08-30 15:41:15 -0300

    config(): setup inicial

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Class_PJ
+# # Repositório de Projetos de Programação
+
+Este é um repositório que contém vários projetos para ajudar meus alunos a aprender programação.
+
+## Projetos
+
+- [Hangman com Docker, Canvas e p5.js](hangman/README.md)
+- [Outro Exemplo](projeto-outro-exemplo/README.md)
+
+## Como usar este repositório
+
+1. Clone o repositório.
+2. Navegue até o diretório do projeto que deseja explorar.
+3. Siga as instruções no README específico do projeto.
+
---
+++ hangman/sketch.js
@@ -0,0 +1,79 @@
+let wordList = ["hangman", "javascript", "programming", "example"]; // Lista de palavras
+let word; // Palavra escolhida
+let hiddenWord = []; // Palavra escondida (com underscores)
+let guessedLetters = []; // Letras já adivinhadas
+let attemptsLeft = 6; // Número de tentativas restantes
+
+function setup() {
+    createCanvas(400, 400);
+    frameRate(2); // Taxa de atualização da tela
+    pickWord();
+    initializeHiddenWord();
+}
+
+function draw() {
+    background(220);
+    displayHiddenWord();
+    displayGuessedLetters();
+    displayAttemptsLeft();
+    checkGameOver();
+}
+
+function pickWord() {
+    word = random(wordList);
+}
+
+function initializeHiddenWord() {
+    for (let i = 0; i < word.length; i++) {
+        hiddenWord[i] = "_";
+    }
+}
+
+function displayHiddenWord() {
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    text(hiddenWord.join(" "), width / 2, height / 2);
+}
+
+function displayGuessedLetters() {
+    textAlign(LEFT);
+    textSize(16);
+    text("Guessed Letters: " + guessedLetters.join(", "), 10, 20);
+}
+
+function displayAttemptsLeft() {
+    textAlign(RIGHT);
+    textSize(16);
+    text("Attempts Left: " + attemptsLeft, width - 10, 20);
+}
+
+function keyPressed() {
+    if (key >= 'a' && key <= 'z' && guessedLetters.indexOf(key) === -1) {
+        guessedLetters.push(key);
+        if (word.includes(key)) {
+            updateHiddenWord(key);
+        } else {
+            attemptsLeft--;
+        }
+    }
+}
+
+function updateHiddenWord(letter) {
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            hiddenWord[i] = letter;
+        }
+    }
+}
+
+function checkGameOver() {
+    if (hiddenWord.join("") === word) {
+        textSize(32);
+        text("You Win!", width / 2, height - 50);
+        noLoop(); // Parar de atualizar o desenho
+    } else if (attemptsLeft === 0) {
+        textSize(32);
+        text("Game Over", width / 2, height - 50);
+        noLoop(); // Parar de atualizar o desenho
+    }
+}
